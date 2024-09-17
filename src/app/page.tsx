a9730e4aed4007c60fb62c55fec924c4e31b01ea
shellyear <boolevafunkce@gmail.com>
2024-09-17 05:17:31 +0200

    [feature] add SingleReview, remove Reviews

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Logo from "@/components/Logo";
 import Container from "@/components/Container";
 import HowItWorks from "@/components/sections/HowItWorks";
 import Introduction from "@/components/sections/Introduction";
-import Reviews from "@/components/sections/Reviews";
 import Link from "next/link";
+import SingleReview from "@/components/sections/SingleReview";
 
 export default function Home() {
   return (
@@ -23,10 +23,16 @@ export default function Home() {
       <main>
         <Introduction />
         <HowItWorks />
-        <Reviews />
+        <SingleReview
+          feedback="This app instantly gives me recipe ideas just by uploading a photo of
+          my ingredients. It’s perfect for busy weeknights when I need quick
+          meal inspiration. Highly recommended!"
+          imgPath="/salta.jpg"
+          name="Salta Sutey"
+          occupation="Home chef"
+        />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </>
   );
 }
