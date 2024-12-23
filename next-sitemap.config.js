ddb970eac5b902c729bf5f43b15fb89a9b944bfe
shellyear <boolevafunkce@gmail.com>
2024-12-23 19:13:09 +0500

    [feature] add postbuild next-sitemap with config

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "http://localhost:3000",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ["/dashboard", "/auth/signin", "/auth/signup"],
+  // ...other options
+};
